d6945b22d777de2a5b00310b5c979d8ed3adf856
Sydney Stokes <sydney.stokes@bison.howard.edu>
2024-12-02 08:20:44 +0000

    Working dot product and used define for matrix size.

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -3,14 +3,17 @@
 #include <pthread.h>
 #include <time.h>
 
+//  Given a 20 x 20 matrix, divide and conquer the matrix arithmetic
+// using a total of 10 threads.
+
+#define MAX 20
+
 int matA[MAX][MAX]; 
 int matB[MAX][MAX]; 
 
 int matSumResult[MAX][MAX];
 int matDiffResult[MAX][MAX]; 
 int matProductResult[MAX][MAX]; 
-
-int MAX;
 
 struct cell{
     int row;
@@ -53,26 +56,47 @@ void* computeSum(void* args) { // pass in the number of the ith thread
 // the cell of matSumResult at the coordinates to the difference of the
 // values at the coordinates of matA and matB.
 void* computeDiff(void* args) { // pass in the number of the ith thread
-    return NULL;
+    struct cell* mycell = (struct cell*)args;
+   int row = mycell -> row;
+   int col = mycell -> col;
+   
+   int matANum = matA[row][col];
+   int matBNum = matB[row][col];
+
+   matDiffResult[row][col] = matANum - matBNum;
 }
 
 // Fetches the appropriate coordinates from the argument, and sets
 // the cell of matSumResult at the coordinates to the inner product
 // of matA and matB.
 void* computeProduct(void* args) { // pass in the number of the ith thread
-    return NULL;
+  struct cell* mycell = (struct cell*)args;
+   int row = mycell -> col;
+   int col = mycell -> row;
+
+   // for spot(0, 1)
+   // A(0, 0)*B(0, 1) + A(0, 1)*B(1, 1) + A(0, 2)*B(2, 1)
+   // spot (r, c)
+   // A(r, 0) *B(0, c) + A(r, 1)*B(1, c) + A(r, 2)*B(2, c)
+   
+    int sum = 0;
+    for(int i = 0; i<MAX; i++){
+        sum += matA[row][i] * matB[i][col];
+    }
+   matProductResult[row][col] = sum;
 }
 
 // Spawn a thread to fill each cell in each result matrix.
 // How many threads will you spawn?
-int main(int argc, char* argv[]) {
+int main() {
     srand(time(0));  // Do Not Remove. Just ignore and continue below.
     
     // 0. Get the matrix size from the command line and assign it to MAX
-    if(argc < 2){
-        printf("Usage: %s <matrix_size>\n", argv[0]);\
-        return 1;
-    }
+    // **Code template and directions conflicting with this instruction. MAX would have to 
+    // be pre-defined for it to work in the way that the code skeleton was set up, each function signature
+    // would have to change from accepting int matrix[MAX][MAX]
+    // The instructions specifically ask for a 20x20 matrix.**
+
     // 1. Fill the matrices (matA and matB) with random values.
     fillMatrix(matA);
     fillMatrix(matB);
@@ -84,9 +108,9 @@ int main(int argc, char* argv[]) {
     printMatrix(matB);
     
     // 3. Create pthread_t objects for our threads.
-    pthread_t sumThreads[Max][Max];
-    pthread_t diffThreads[Max][Max];
-    pthread_t productThreads[Max][Max];
+    pthread_t sumThreads[MAX][MAX];
+    pthread_t diffThreads[MAX][MAX];
+    pthread_t productThreads[MAX][MAX];
     
     // 4. Create a thread for each cell of each matrix operation.
     // 
